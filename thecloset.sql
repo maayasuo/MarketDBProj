905222efd6f9ba0c256e41aa6e18e0f0cca612ca
maayasuo <thean2000@gmail.com>
2022-11-19 00:47:10 +0700

    Update database

--- thecloset.sql
+++ thecloset.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 18, 2022 at 06:16 PM
+-- Generation Time: Nov 18, 2022 at 06:46 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -18,70 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `thecloset`
+-- Database: `proj`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cart`
---
-
-CREATE TABLE `cart` (
-  `pID` int(3) NOT NULL,
-  `cartID` varchar(6) NOT NULL,
-  `cusername` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product`
---
-
-CREATE TABLE `product` (
-  `pID` int(3) NOT NULL,
-  `pname` varchar(25) NOT NULL,
-  `pimage` varchar(350) NOT NULL,
-  `price` int(4) NOT NULL,
-  `isfeature` char(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `product`
---
-
-INSERT INTO `product` (`pID`, `pname`, `pimage`, `price`, `isfeature`) VALUES
-(101, 'White Shirt', 'asset/product/top/t1', 299, 'a'),
-(102, 'Red Shirt', 'asset/product/top/t2', 299, 'a'),
-(103, 'Blue Silk Shirt', 'asset/product/top/t3', 369, 'a'),
-(201, 'Black Jean', 'asset/product/pants/p1', 499, 'b'),
-(202, 'Short Jean', 'asset/product/pants/p2', 379, 'b'),
-(301, 'Sneaker', 'asset/product/shoes/s1', 679, 'c'),
-(302, 'Leather Shoe', 'asset/product/shoes/s1', 879, 'c'),
-(401, 'Black Hat', 'asset/product/accessories/a1', 159, 'd'),
-(402, 'Leather Belt', 'asset/product/accessories/a2', 429, 'd');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product_type`
---
-
-CREATE TABLE `product_type` (
-  `ptype` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `promocode`
---
-
-CREATE TABLE `promocode` (
-  `codeID` varchar(10) NOT NULL,
-  `expDate` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -104,48 +42,11 @@ CREATE TABLE `user` (
 --
 
 --
--- Indexes for table `cart`
---
-ALTER TABLE `cart`
-  ADD PRIMARY KEY (`cartID`),
-  ADD KEY `FK_username` (`cusername`),
-  ADD KEY `FK_pID` (`pID`);
-
---
--- Indexes for table `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`pID`);
-
---
--- Indexes for table `product_type`
---
-ALTER TABLE `product_type`
-  ADD PRIMARY KEY (`ptype`);
-
---
--- Indexes for table `promocode`
---
-ALTER TABLE `promocode`
-  ADD PRIMARY KEY (`codeID`);
-
---
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`userID`),
   ADD UNIQUE KEY `username` (`username`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `cart`
---
-ALTER TABLE `cart`
-  ADD CONSTRAINT `FK_pID` FOREIGN KEY (`pID`) REFERENCES `product` (`pID`),
-  ADD CONSTRAINT `FK_username` FOREIGN KEY (`cusername`) REFERENCES `user` (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
